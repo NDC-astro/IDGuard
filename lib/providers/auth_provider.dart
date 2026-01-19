32bbaa5060311f57e5caa5d1a9166441bd148a48
crescens <cres.djas25@gmail.com>
2026-01-19 23:38:08 +0000

    Add auth provider

---
+++ lib/providers/auth_provider.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/foundation.dart';
+import '../models/document.dart';
+import '../services/storage_service.dart';
+
+
+/// Provider for authentication state
+class AuthProvider with ChangeNotifier {
+  bool _isAuthenticated = false;
+  bool _isBiometricAvailable = false;
+  bool _isBiometricEnabled = false;
+
+  bool get isAuthenticated => _isAuthenticated;
+  bool get isBiometricAvailable => _isBiometricAvailable;
+  bool get isBiometricEnabled => _isBiometricEnabled;
+
+  void setAuthenticated(bool value) {
+    _isAuthenticated = value;
+    notifyListeners();
+  }
+
+  void setBiometricAvailable(bool value) {
+    _isBiometricAvailable = value;
+    notifyListeners();
+  }
+
+  void setBiometricEnabled(bool value) {
+    _isBiometricEnabled = value;
+    notifyListeners();
+  }
+
+  void logout() {
+    _isAuthenticated = false;
+    notifyListeners();
+  }
+}
