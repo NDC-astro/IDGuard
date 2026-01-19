ee846dc96bb9f9e25e32aeacd6461cbe6e319661
crescens <cres.djas25@gmail.com>
2026-01-19 23:38:38 +0000

    ÃImplement provider for theme

---
+++ lib/providers/theme_provider.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import '../models/document.dart';
+import '../services/storage_service.dart';
+
+/// Provider for theme management
+class ThemeProvider with ChangeNotifier {
+  ThemeMode _themeMode = ThemeMode.system;
+
+  ThemeMode get themeMode => _themeMode;
+  bool get isDarkMode => _themeMode == ThemeMode.dark;
+
+  void setThemeMode(ThemeMode mode) {
+    _themeMode = mode;
+    notifyListeners();
+  }
+
+  void toggleTheme() {
+    _themeMode = _themeMode == ThemeMode.light
+        ? ThemeMode.dark
+        : ThemeMode.light;
+    notifyListeners();
+  }
+}
